a02ee62c236b3cdfcf43a945b3a93baaf861e535
josheenyen99 <josheen@ualberta.ca>
2024-10-07 22:31:51 -0600

    fix transparency inconsistency

--- lua/josheen/color.lua
+++ lua/josheen/color.lua
@@ -1,2 +1,9 @@
 vim.cmd("colorscheme kanagawa")
 
+vim.opt.background = "dark"
+
+vim.cmd([[
+    highlight Normal guibg=NONE ctermbg=NONE
+    highlight NonText guibg=NONE ctermbg=NONE
+    highlight LineNr guibg=NONE ctermbg=NONE
+]])
