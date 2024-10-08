35fc3e2b97673665cf09a1bcb693509d1a591595
Jo-Sheen Yen <jo-sheen.yen@garmin.com>
2024-10-08 17:39:02 -0600

    allow transparent backgroudn
    Change-Id: I9d7942120becea30e87ef04653b8f9aee4235909

--- lua/josheen/color.lua
+++ lua/josheen/color.lua
@@ -7,6 +7,25 @@ end
 -- Desired colorscheme
 local colorscheme = 'catppuccin'
 
+local colors = require("catppuccin.palettes").get_palette()
+
+require("catppuccin").setup({
+    flavour = "mocha",
+    transparent_background = true,
+    styles = {
+        functions = { "italic" },
+        keywords = { "bold" },
+    },
+    integrations = {
+        nvimtree = true,
+        mason = true,
+    },
+    custom_highlights = {
+        PanelHeading = { fg = colors.lavender, bold = true },
+        IndentBlanklineContextChar = { fg = colors.overlay0 },
+    },
+})
+
 -- Check if the colorscheme exists before setting it
 if colorscheme_exists(colorscheme) then
     vim.cmd('colorscheme ' .. colorscheme)
@@ -14,10 +33,9 @@ else
     vim.cmd('colorscheme peaksea')
 end
 
--- vim.opt.background = "dark"
-
--- vim.cmd([[
---     highlight Normal guibg=NONE ctermbg=NONE
---     highlight NonText guibg=NONE ctermbg=NONE
---     highlight LineNr guibg=NONE ctermbg=NONE
--- ]])
+vim.opt.background = "dark"
+vim.cmd([[
+    highlight Normal guibg=NONE ctermbg=NONE
+    highlight NonText guibg=NONE ctermbg=NONE
+    highlight LineNr guibg=NONE ctermbg=NONE
+]])
