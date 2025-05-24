69ec1f7acd63dd114801b75390d67396c2c9c8cb
josheenyen99 <josheen@ualberta.ca>
2025-05-24 00:45:56 -0600

    add keymap for bd

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>e", '<cmd> NvimTreeToggle <CR>')
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("n", "<leader>bd", '<cmd>bd<cr>')
 require('gitsigns').setup {
     on_attach = function(bufnr)
     local gitsigns = require('gitsigns')
